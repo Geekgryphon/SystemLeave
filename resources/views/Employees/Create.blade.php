07543126b552199facb36cd1fb56ba628a1ed059
Geekgryphon <geekgryphon@gmail.com>
2024-09-12 17:15:44 +0800

    Init

---
+++ resources/views/Employees/Create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Employees - Account, Name, Password, Email, BirthDay,  CityID, CityAreaID, StreetID, Address, StartDate, EndDate
+    工作歷程 - Account, JobID, BeginDate, EndDate
+    職位 Job - Title, EngTitle, Used, Seq
+
+    縣市 -  PostalCode, Name, Used, Seq 
+    鄉鎮 -  PostalCode, CityID, Name, Used, Seq
+    街道 -  CityAreaID, Name, Used, Seq
+    
+    請假單 - LeaveFormNo, LeaveKindID, LeaveFormReason, ApplyEmployee, BeginTime, EndTime
+    假單類別 - Name, Seq, Used
+    假單簽核流程關卡 LeaveFormNo, Step,  SignTime, Status, Memo, Attatch
+    簽核狀態 -
+
+    簽到退
+    計算該月的上班時數
+
+</body>
+</html>
